451faf568925ddfa2429bfc97dbb28466f7e8597
Luka Stromajer <str240266@spengergasse.at>
2026-09-21 21:06:09 +0200

    Implement updateNumbers and updateScore functions to manage game state and display results; enhance user feedback with dynamic score updates and outcome messages.

--- src/modules/logic.js
+++ src/modules/logic.js
@@ -1,11 +1,18 @@
-// make a function that types out the three dots at "Hidden number is ..." continously, with a delay of 500ms between each dot, and then resets after the third dot. Use setInterval and clearInterval to do this twin, you can use a variable to keep track of the number of dots currently displayed, and update the text content of the element accordingly
-
 // select all necessary components that we will change dynamically
 const lowerButton = document.querySelector('.lower-button');
 const higherButton = document.querySelector('.higher-button');
 
 const currentNumber = document.querySelector('.current-number').textContent;
+const currentNumberDiv = document.querySelector('.current-number');
+
 const hiddenNumber = document.querySelector('.hidden-number').textContent;
+const hiddenNumberDiv = document.querySelector('.hidden-number');
+
+const resultDiv = document.querySelector('.result');
+const scoreDiv = document.querySelector('.score');
+
+let result = '';
+let score = '';
 
 // lets watch if the player pressed a button:
 lowerButton.addEventListener(
@@ -35,5 +42,39 @@ function compareNumbers(currentNumber, hiddenNumber, whichButton) {
     outcome = 0;
   }
 
-  return outcome;
+  updateNumbers(outcome);
 }
+
+function updateNumbers(outcome) {
+  // get a random number between 0 and 100 and set it as the current number
+  const newCurrentNumber = Math.floor(Math.random() * 101);
+  currentNumber.textContent = newCurrentNumber;
+
+  // get a random number between 0 and 100 and set it as the hidden number
+  const newHiddenNumber = Math.floor(Math.random() * 101);
+  hiddenNumber.textContent = newHiddenNumber;
+
+  updateScore(outcome, newHiddenNumber, newCurrentNumber);
+}
+
+function updateScore(outcome, newHiddenNumber, newCurrentNumber) {
+  if (outcome === 1) {
+    score++;
+    scoreDiv.textContent = `Score: ${score}`;
+    updateResult(outcome, newHiddenNumber, newCurrentNumber);
+  } else {
+    updateResult(outcome, newHiddenNumber, newCurrentNumber);
+  }
+}
+
+function updateResult(outcome, newHiddenNumber, newCurrentNumber) {
+  if (outcome === 1) {
+    resultDiv.textContent = 'YOOO LETS GOOO';
+  } else {
+    resultDiv.textContent = 'FUCKKKK';
+  }
+}
+
+// make a updateNumbers function that updates the current number and hidden number after each round
+// make a updateScore function that updates the scoreDiv with the current score
+// make it so that every time the outcome comes out, the result in the resultDiv "bounces" in scale, so its obvious if you won or lost and not just a static text change, because if it were static, you wouldnt even be able to see if you got it wrong or right twice in a row
